2b1abc94f573aeafa1889d54d25ed07b7f33e9f9
Utkarsh Shukla <96052149+utkrsh-shukla@users.noreply.github.com>
2025-01-19 18:24:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+page_bg_img = '''
+<style>
+body {
+background-image: url("Jiraya.png");
+background-size: cover;
+}
+</style>
+'''
 
+st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title("🧠 Mental-Health-Risk-Prediction-System")
 st.write(
     "On it , will be done within 3 days"
