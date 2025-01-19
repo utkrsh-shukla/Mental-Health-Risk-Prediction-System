e520a0face6d22f556f0a1946d8299ff41e7d7ba
Utkarsh Shukla <96052149+utkrsh-shukla@users.noreply.github.com>
2025-01-19 18:37:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,4 @@
 import streamlit as st
-# Path to the background image
-image_path = 'https://github.com/utkrsh-shukla/Mental-Health-Risk-Prediction-System/blob/main/Jiraya.png'
-
-# HTML/CSS for background image
-
-
-# Apply the background style
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
-    }
-   .sidebar .sidebar-content {
-        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 
 st.title("🧠 Mental-Health-Risk-Prediction-System")
 st.write(
