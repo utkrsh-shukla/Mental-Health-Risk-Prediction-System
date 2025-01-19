c109f8dcf06794f1a96f78a66376f6a9b80f82b4
Utkarsh Shukla <96052149+utkrsh-shukla@users.noreply.github.com>
2025-01-19 11:27:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🧠 Mental-Health-Risk-Prediction-System")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "On it , will be done within 3 days"
 )
